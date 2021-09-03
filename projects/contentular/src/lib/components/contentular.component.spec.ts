cdd96c516e9c834216106c613b5ff3ee01e29fb4
Sascha Wolff <sw@saschawolff.de>
2021-09-03 15:11:41 +0200

    Upgrade to angular 12

--- projects/contentular/src/lib/components/contentular.component.spec.ts
+++ projects/contentular/src/lib/components/contentular.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ContentularComponent } from './contentular.component';
 
@@ -6,7 +6,7 @@ describe('ContentularComponent', () => {
   let component: ContentularComponent;
   let fixture: ComponentFixture<ContentularComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ContentularComponent ]
     })
